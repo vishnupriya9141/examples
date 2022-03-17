98457525b78d185ad9dd0ce2cf02c667d50ef0bc
vishnupriya9141 <95031990+vishnupriya9141@users.noreply.github.com>
2022-03-17 11:19:44 +0530

    added description

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
-        Syatem.out.println("Hello");
+        System.out.println("Hello"); 
+        Syatem.out.println("Welcome to DevOps lab!");
     }
 }
