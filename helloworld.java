9802de586b4826c51f1691e42f2b593492a47610
vishnupriya9141 <95031990+vishnupriya9141@users.noreply.github.com>
2022-06-16 16:30:56 +0530

    Update helloworld.java
    added line

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello"); 
         Syatem.out.println("Welcome to DevOps lab!");
+        System.out.println("Hello welcome ro r");
     }
 }
